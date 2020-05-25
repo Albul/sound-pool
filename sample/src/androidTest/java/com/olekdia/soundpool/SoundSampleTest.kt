6e81e1375224d058ec0ee48f318a003b006d745d
albul <oleks.albul@gmail.com>
2020-05-25 16:10:40 +0300

    Add SoundSampleTest

--- sample/src/androidTest/java/com/olekdia/soundpool/SoundSampleTest.kt
+++ sample/src/androidTest/java/com/olekdia/soundpool/SoundSampleTest.kt
@@ -11,11 +11,11 @@ import org.junit.Test
 
 @LargeTest
 class SoundSampleTest {
+
     @get:Rule
     var activityTestRule = ActivityTestRule(MainActivity::class.java)
 
     private val context: Context get() = activityTestRule.activity
-
 
     @Test
     fun createSample_initialStateCorrect() {
@@ -30,20 +30,89 @@ class SoundSampleTest {
 
     @Test
     fun loadStaticSample_stateCorrect() {
-        val sample = SoundSample(12, 123, false)
-        val metadata = SoundSampleMetadata(12, R.raw.bg_sea_retain, null)
+        val sample = SoundSample(12, 1234567, true)
+        val metadata = SoundSampleMetadata(12, R.raw.sec_tick_bird_ouzel, null)
         val descr = SoundSampleDescriptor(context, metadata)
 
-        sample.load(
+        val success = sample.load(
             descr.fileDescriptor,
             descr.fileOffset,
             descr.fileSize
         )
+        assertTrue(success)
         assertFalse(sample.isClosed)
         assertTrue(sample.isLoaded())
         assertFalse(sample.isPlaying())
         assertFalse(sample.isPaused())
         assertTrue(sample.isStopped())
         assertEquals(12, sample.id)
+    }
+
+    @Test
+    fun loadNonStaticSample_stateCorrect() {
+        val sample = SoundSample(12, 1234567, false)
+        val metadata = SoundSampleMetadata(12, R.raw.bg_sea_retain, null)
+        val descr = SoundSampleDescriptor(context, metadata)
+
+        val success = sample.load(
+            descr.fileDescriptor,
+            descr.fileOffset,
+            descr.fileSize
+        )
+        assertTrue(success)
+        assertFalse(sample.isClosed)
+        assertTrue(sample.isLoaded())
+        assertFalse(sample.isPlaying())
+        assertFalse(sample.isPaused())
+        assertTrue(sample.isStopped())
+        assertEquals(12, sample.id)
+    }
+
+    @Test
+    fun loadSample_closeSample_stateCorrect() {
+        val sample = SoundSample(12, 1234567, false)
+        val metadata = SoundSampleMetadata(12, R.raw.bg_sea_retain, null)
+        val descr = SoundSampleDescriptor(context, metadata)
+
+        val success = sample.load(
+            descr.fileDescriptor,
+            descr.fileOffset,
+            descr.fileSize
+        )
+        assertTrue(success)
+        assertFalse(sample.isClosed)
+        assertTrue(sample.isLoaded())
+
+        sample.close()
+        assertTrue(sample.isClosed)
+        assertFalse(sample.isLoaded())
+
+        assertFalse(sample.isPlaying())
+        assertFalse(sample.isPaused())
+        assertFalse(sample.isStopped())
+    }
+
+    @Test
+    fun loadSample_nonAudioFile_stateCorrect() {
+        val sample = SoundSample(12, 1234567, false)
+        val metadata = SoundSampleMetadata(12, R.raw.design_patterns_pdf, null)
+        val descr = SoundSampleDescriptor(context, metadata)
+
+        val success = sample.load(
+            descr.fileDescriptor,
+            descr.fileOffset,
+            descr.fileSize
+        )
+        assertFalse(success)
+        assertTrue(sample.isClosed)
+        assertFalse(sample.isLoaded())
+
+        sample.close()
+        assertTrue(sample.isClosed)
+        assertFalse(sample.isLoaded())
+
+        assertFalse(sample.isPlaying())
+        assertFalse(sample.isPaused())
+        assertFalse(sample.isStopped())
     }
 }
